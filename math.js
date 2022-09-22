7258a5ba71ae1257c5cb9ded2c7d7c062eaa4b8f
Davy Sproule <dsproule@vt.edu>
2022-09-22 06:45:50 -0400

    Add operate function

--- math.js
+++ math.js
@@ -14,8 +14,26 @@ function divide(a,b){
     return a / b;
 }
 
+function operate(operator, a, b){
+    if (operator === '+'){
+        return add(a,b);
+    }
+    else if (operator === '-'){
+        return subtract(a,b);
+    }
+    else if (operator === '*'){
+        return multiply(a,b);
+    }
+    else if (operator === '/'){
+        return divide(a,b);
+    }
+    else {
+        return 'Operation Error'
+    }
+}
 
-console.log(add(5,10))
-console.log(subtract(10,2))
-console.log(multiply(5,5.25))
-console.log(divide(9,0))
+console.log(operate('+',2,4))
+console.log(operate('-',8, 1))
+console.log(operate('*',6, 3))
+console.log(operate('/',10,2))
+console.log(operate('w',2,4))
