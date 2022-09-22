454e33642cce12fc30663175cb8729120c77df8d
Davy Sproule <dsproule@vt.edu>
2022-09-22 06:37:17 -0400

    Add basic math functions

---
+++ math.js
@@ -0,0 +1,21 @@
+function add(a,b){
+    return a + b;
+};
+
+function subtract(a,b){
+    return a - b;
+};
+
+function multiply(a,b){
+    return a * b;
+};
+
+function divide(a,b){
+    return a / b;
+}
+
+
+console.log(add(5,10))
+console.log(subtract(10,2))
+console.log(multiply(5,5.25))
+console.log(divide(9,0))
