62dc5e2e8f2741a26a617476779e41a4c213a04e
Davy Sproule <dsproule@vt.edu>
2022-09-30 07:48:42 -0400

    Add operation functionality

--- math.js
+++ math.js
@@ -1,13 +1,55 @@
 
+//Initialize variables
+let clearOnNext = false;
+let operator = '';
+let numFirst = '';
+let numSecond = '';
+let operatorEvaluation = false;
+const display = document.getElementById('display');
+const equals = document.getElementById('equals');
 
-document.querySelectorAll('.number').forEach((button)=>{
-    button.addEventListener('click', ()=>{
-        display = document.getElementById('display');
-        display.innerHTML += button.innerHTML;
+// Update display when numbers clicked
+document.querySelectorAll('.number').forEach((button) => {
+    button.addEventListener('click', ()=>{ 
+        //if statement handles clearing display on next number if operation is occuring
+        if (!clearOnNext){
+            display.textContent += button.textContent;
+        }
+        else {
+            display.textContent = '';
+            display.textContent += button.textContent;
+            clearOnNext = false;
+        }
     })
 });
-   
 
+// On click of operator stores value, if operator already clicked evaluates first operation
+document.querySelectorAll('.operator').forEach((button) => {
+    button.addEventListener('click', () => { 
+        if(!operatorEvaluation){
+            numFirst = display.textContent;
+            operator = button.textContent;
+            clearOnNext = true;
+            operatorEvaluation = true;
+        }
+        else{
+            numSecond = display.textContent;
+            display.textContent = operate(operator, Number(numFirst), Number(numSecond));
+            clearOnNext = true;
+            numFirst = display.textContent;
+            operator = button.textContent;
+        }           
+    })
+});
+
+// Equals button operation
+equals.addEventListener('click', () => {
+    numSecond = display.textContent;
+    display.textContent = operate(operator, Number(numFirst), Number(numSecond));
+    operator = '';
+})
+
+// Math functions
 function add(a,b){
     return a + b;
 };
